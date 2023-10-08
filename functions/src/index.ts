864870c96cdeeedbea89495e4ab4bc01e75b9272
dark_king <manas.gowda04@yahoo.com>
2023-10-08 14:59:52 -0700

    final changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -15,7 +15,7 @@ export const helloWorld = onRequest(async (request, response) => {
     const model = "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4";
     const input = {
         prompt: "coloring book",
-        image: request.body.img,
+        image: request.query.img,
     };
     const output = await replicate.run(model, { input });
 
